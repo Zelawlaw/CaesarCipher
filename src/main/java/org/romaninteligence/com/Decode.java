7e5751bd333d7ea47c2e4c5df445cefc711f5ff2
Lawrence Karani <lawrencekarani@gmail.com>
2023-04-16 22:19:39 +0300

    added logic to decodeIt method

--- src/main/java/org/romaninteligence/com/Decode.java
+++ src/main/java/org/romaninteligence/com/Decode.java
@@ -2,13 +2,53 @@ package org.romaninteligence.com;
 
 import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Decode {
+    static final String REG_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    Map<Integer,Character> letterposition;
+
+    public Decode(){
+        //initialize letterposition on object creation
+        this.letterposition = new HashMap<>();
+        for(int i =0;i<REG_ALPHABET.length();i++){
+            this.letterposition.put((REG_ALPHABET.length()-1)-i,REG_ALPHABET.charAt(i));
+        }
+    }
+
+    public String decodeIt(String encodedmessage, int key) {
+
+        String encodedMessage = " ";
+        StringBuilder sb = new StringBuilder();
+        char[] messagechars = encodedmessage.toCharArray();
+        for (char varchar : messagechars) {
+            if (!Character.isLetter(varchar)) {
+                //not a letter. append to sb and continue
+                sb.append(varchar);
+                continue;
+            } else {
+                //it's a letter. encode
+                //mark lowercase flag
+                boolean isLowerCase = Character.isLowerCase(varchar);
+                varchar = isLowerCase? Character.toUpperCase(varchar) :varchar;
+                int currentposition = 'Z' - varchar ;
+                int decodedposition = (currentposition + key) % 26;
+                if(isLowerCase)
+                {
+                    sb.append(Character.toLowerCase(this.letterposition.get(decodedposition)));
+                }
+                else {
+                    sb.append(this.letterposition.get(decodedposition));
+                }
+
+            }
+
+        }
 
 
-
-    public String decodeIt(String stringtodecode, int key) {
-
+        return sb.toString();
 
     }
 }
