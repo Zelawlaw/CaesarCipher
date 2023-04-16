9349c6866e858881bf98f56ae653f3becfbcc886
Lawrence Karani <lawrencekarani@gmail.com>
2023-04-16 21:50:20 +0300

    added code to encodeIt method

--- src/main/java/org/romaninteligence/com/Encode.java
+++ src/main/java/org/romaninteligence/com/Encode.java
@@ -2,13 +2,52 @@ package org.romaninteligence.com;
 
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Encode {
+       static final String REG_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+       Map<Integer,Character> letterposition;
+
+       public Encode (){
+      //initialize letterposition on object creation
+      this.letterposition = new HashMap<>();
+      for(int i =0;i<REG_ALPHABET.length();i++){
+        this.letterposition.put(i,REG_ALPHABET.charAt(i));
+      }
+       }
+
+    public String encodeIt(String message, int key) {
+        String encodedMessage = " ";
+        StringBuilder sb = new StringBuilder();
+        char[] messagechars = message.toCharArray();
+        for (char varchar : messagechars) {
+            if (!Character.isLetter(varchar)) {
+                //not a letter. append to sb and continue
+                sb.append(varchar);
+                continue;
+            } else {
+                //it's a letter. encode
+            //mark lowercase flag
+            boolean isLowerCase = Character.isLowerCase(varchar);
+            varchar = isLowerCase? Character.toUpperCase(varchar) :varchar;
+            System.out.println(this.letterposition);
+            int currentposition = varchar - 'A';
+            int encodedposition = (currentposition + key) % 26;
+            if(isLowerCase)
+            {
+               sb.append(Character.toLowerCase(this.letterposition.get(encodedposition)));
+            }
+            else {
+                sb.append(this.letterposition.get(encodedposition));
+            }
+
+            }
+
+        }
 
 
-    public String encodeIt(String message, int key){
-      String encodedMessage= " ";
-
-      return encodedMessage;
+        return sb.toString();
     }
 }
--- src/test/java/org/romaninteligence/com/EncodeTest.java
+++ src/test/java/org/romaninteligence/com/EncodeTest.java
@@ -1,5 +1,6 @@
 package org.romaninteligence.com;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class EncodeTest {
 
     @Test
-    void encodeIt() {
+    @DisplayName("It should encode String Fox String with key 23")
+    void encodeFoxString() {
+        Encode encodeObj = new Encode();
+        int key = 23;
+        String stringtoEncode = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+        String expectedEncoded ="QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD";
+        assertEquals(expectedEncoded,encodeObj.encodeIt(stringtoEncode,key));
     }
+
+    @Test
+    @DisplayName("It should encode String Hi String with key 2")
+    void encodeHiString() {
+        Encode encodeObj = new Encode();
+        int key = 2;
+        String stringtoEncode = "HI";
+        String expectedEncoded ="JK";
+        assertEquals(expectedEncoded,encodeObj.encodeIt(stringtoEncode,key));
+    }
+
+    @Test
+    @DisplayName("It should encode String Hi String with key 20")
+    void encodeHiString2() {
+        Encode encodeObj = new Encode();
+        int key = 20;
+        String stringtoEncode = "HI";
+        String expectedEncoded ="BC";
+        assertEquals(expectedEncoded,encodeObj.encodeIt(stringtoEncode,key));
+    }
+
 }
