10d614387e8089c77c014e42df841c82f4518688
Lawrence Karani <lawrencekarani@gmail.com>
2023-04-16 21:56:09 +0300

    added additional edge test cases

--- src/test/java/org/romaninteligence/com/EncodeTest.java
+++ src/test/java/org/romaninteligence/com/EncodeTest.java
@@ -28,12 +28,32 @@ class EncodeTest {
     }
 
     @Test
-    @DisplayName("It should encode String Hi String with key 20")
+    @DisplayName("It should encode String HI String with key 20")
     void encodeHiString2() {
         Encode encodeObj = new Encode();
         int key = 20;
         String stringtoEncode = "HI";
         String expectedEncoded ="BC";
+        assertEquals(expectedEncoded,encodeObj.encodeIt(stringtoEncode,key));
+    }
+
+    @Test
+    @DisplayName("It should encode String hi String with key 20")
+    void encodeHiString2LowerCase() {
+        Encode encodeObj = new Encode();
+        int key = 20;
+        String stringtoEncode = "hi";
+        String expectedEncoded ="bc";
+        assertEquals(expectedEncoded,encodeObj.encodeIt(stringtoEncode,key));
+    }
+
+    @Test
+    @DisplayName("It should encode String hi%lolo (Special Character) String with key 2")
+    void encodeHiStringSpecialCharacterLowerCase() {
+        Encode encodeObj = new Encode();
+        int key = 2;
+        String stringtoEncode = "hi%lolo";
+        String expectedEncoded ="jk%nqnq";
         assertEquals(expectedEncoded,encodeObj.encodeIt(stringtoEncode,key));
     }
 
