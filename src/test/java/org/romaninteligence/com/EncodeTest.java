be08d4e262ffaec5c4e0cc9a9e6f189862c4d617
Lawrence Karani <lawrencekarani@gmail.com>
2023-04-22 10:24:17 +0300

    refactor tests to re-use one object as opposed to creating one for each test

--- src/test/java/org/romaninteligence/com/EncodeTest.java
+++ src/test/java/org/romaninteligence/com/EncodeTest.java
@@ -1,16 +1,33 @@
 package org.romaninteligence.com;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class EncodeTest {
+
+    private Encode encodeObj;
+
+    @BeforeAll
+    void setupBeforeTests() {
+        encodeObj = new Encode();
+        assertNotNull(encodeObj);
+    }
+
+    @Test
+    void checkInitialization() {
+        //checks if decode Object was setup correctly.
+    }
 
     @Test
     @DisplayName("It should encode String Fox String with key 23")
     void encodeFoxString() {
-        Encode encodeObj = new Encode();
+
         int key = 23;
         String stringtoEncode = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
         String expectedEncoded = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD";
@@ -22,7 +39,7 @@ class EncodeTest {
     @Test
     @DisplayName("It should encode String Hi String with key 2")
     void encodeHiString() {
-        Encode encodeObj = new Encode();
+
         int key = 2;
         String stringtoEncode = "HI";
         String expectedEncoded = "JK";
@@ -34,7 +51,7 @@ class EncodeTest {
     @Test
     @DisplayName("It should encode String HI String with key 20")
     void encodeHiString2() {
-        Encode encodeObj = new Encode();
+
         int key = 20;
         String stringtoEncode = "HI";
         String expectedEncoded = "BC";
@@ -46,7 +63,7 @@ class EncodeTest {
     @Test
     @DisplayName("It should encode String hi String with key 20")
     void encodeHiString2LowerCase() {
-        Encode encodeObj = new Encode();
+
         int key = 20;
         String stringtoEncode = "hi";
         String expectedEncoded = "bc";
@@ -58,7 +75,7 @@ class EncodeTest {
     @Test
     @DisplayName("It should encode String hi%lolo (Special Character) String with key 2")
     void encodeHiStringSpecialCharacterLowerCase() {
-        Encode encodeObj = new Encode();
+
         int key = 2;
         String stringtoEncode = "hi%lolo";
         String expectedEncoded = "jk%nqnq";
